781ec38ba7afe538488ed704c4f0c7dba1e5ec2f
Chris Miles <miles.chris@gmail.com>
2013-10-02 20:44:46 +1000

    Improved springy attachments line drawing dash size calculations.

--- DynamicsXRay/DynamicsXRay/DXRDynamicsXRayItemAttachment+DXRDrawing.m
+++ DynamicsXRay/DynamicsXRay/DXRDynamicsXRayItemAttachment+DXRDrawing.m
@@ -29,8 +29,9 @@
         // Spring attachments are drawn as dashed lines
         CGFloat lineLength = DXRCGPointDistance(self.anchorPoint, self.attachmentPoint);
         CGFloat itemLength = (self.length > 0 ? self.length : 1.0f);
-        CGFloat lineDiff = lineLength / itemLength;
-        CGFloat dashSize = fmaxf(0.5f, 3.0f * lineDiff);
+        CGFloat lineRatio = lineLength / itemLength;
+        CGFloat dashBaseSize = fminf(3.0f, itemLength / 10.0f);
+        CGFloat dashSize = fmaxf(0.5f, dashBaseSize * lineRatio);
         const CGFloat dashPattern[2] = {dashSize, dashSize};
         CGContextSetLineDash(context, 3, dashPattern, 2);
     }
