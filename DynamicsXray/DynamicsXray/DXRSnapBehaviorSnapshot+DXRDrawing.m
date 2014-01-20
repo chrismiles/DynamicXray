83749799ea8dbfae7d4cb8f638d1bd05709029b2
Chris Miles <miles.chris@gmail.com>
2014-01-21 07:42:51 +1100

    Draw arrow head on snap visualisation.

--- DynamicsXray/DynamicsXray/DXRSnapBehaviorSnapshot+DXRDrawing.m
+++ DynamicsXray/DynamicsXray/DXRSnapBehaviorSnapshot+DXRDrawing.m
@@ -15,11 +15,25 @@
     CGPoint itemCenter = self.itemCenter;
     CGPoint anchorPoint = self.anchorPoint;
 
+    CGFloat xDiff = self.anchorPoint.x - self.itemCenter.x;
+    CGFloat yDiff = self.anchorPoint.y - self.itemCenter.y;
+    CGFloat lineLength = sqrtf(xDiff*xDiff + yDiff*yDiff);
+    CGFloat arrowHeadLength = lineLength * 0.2f;
+    CGFloat lineAngle = atan2f(yDiff, xDiff);
+    CGFloat arrowHeadAngle1 = lineAngle + (150.0f * M_PI / 180.0f);
+    CGFloat arrowHeadAngle2 = lineAngle - (150.0f * M_PI / 180.0f);
+    CGPoint arrowHeadEndPoint1 = CGPointMake(anchorPoint.x + cosf(arrowHeadAngle1)*arrowHeadLength, anchorPoint.y + sinf(arrowHeadAngle1)*arrowHeadLength);
+    CGPoint arrowHeadEndPoint2 = CGPointMake(anchorPoint.x + cosf(arrowHeadAngle2)*arrowHeadLength, anchorPoint.y + sinf(arrowHeadAngle2)*arrowHeadLength);
+
     const CGFloat dashPattern[2] = {3.0f, 3.0f};
     CGContextSetLineDash(context, 3, dashPattern, 2);
 
     CGContextMoveToPoint(context, itemCenter.x, itemCenter.y);
     CGContextAddLineToPoint(context, anchorPoint.x, anchorPoint.y);
+
+    CGContextAddLineToPoint(context, arrowHeadEndPoint1.x, arrowHeadEndPoint1.y);
+    CGContextMoveToPoint(context, anchorPoint.x, anchorPoint.y);
+    CGContextAddLineToPoint(context, arrowHeadEndPoint2.x, arrowHeadEndPoint2.y);
 
     CGContextStrokePath(context);
 }
