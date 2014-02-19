be36fd5689e302110972ba29d2285df20d19010c
Chris Miles <miles.chris@gmail.com>
2014-02-20 07:59:05 +1100

    Updated the gravity behavior visualisation.

--- DynamicsXray/DynamicsXray/DXRGravityBehaviorSnapshot+DXRDrawing.m
+++ DynamicsXray/DynamicsXray/DXRGravityBehaviorSnapshot+DXRDrawing.m
@@ -9,8 +9,8 @@
 #import "DXRGravityBehaviorSnapshot+DXRDrawing.h"
 @import UIKit;
 
-static CGFloat const circleDiameter = 30.0f;
-static CGFloat const arrowHeadPointOffsetAngle = 0.2f;
+static CGFloat const circleDiameter = 40.0f;
+static CGFloat const arrowHeadPointOffsetAngle = 0.25f;
 
 
 @implementation DXRGravityBehaviorSnapshot (DXRDrawing)
@@ -21,39 +21,35 @@ static CGFloat const arrowHeadPointOffsetAngle = 0.2f;
     CGPoint circleCenter = CGPointMake(CGRectGetMidX(circleFrame), CGRectGetMidY(circleFrame));
 
     CGFloat angle = self.angle;
-    CGPoint arrowEndPoint = CGPointMake(circleCenter.x + circleDiameter * cosf(angle), circleCenter.y + circleDiameter * sinf(angle));
-    CGFloat arrowHeadPointPointRadius = circleDiameter * 0.8f;
-    CGPoint arrowHeadPoint1 = CGPointMake(circleCenter.x + arrowHeadPointPointRadius * cosf(angle-arrowHeadPointOffsetAngle),
-                                          circleCenter.y + arrowHeadPointPointRadius * sinf(angle-arrowHeadPointOffsetAngle));
-    CGPoint arrowHeadPoint2 = CGPointMake(circleCenter.x + arrowHeadPointPointRadius * cosf(angle+arrowHeadPointOffsetAngle),
-                                          circleCenter.y + arrowHeadPointPointRadius * sinf(angle+arrowHeadPointOffsetAngle));
+    CGFloat arrowRadius = circleDiameter / 2.0f - 1.0f;
+    CGPoint arrowEndPoint = CGPointMake(circleCenter.x + arrowRadius * cosf(angle), circleCenter.y + arrowRadius * sinf(angle));
+    CGFloat arrowHeadPointEndRadius = arrowRadius * 0.7f;
+    CGPoint arrowHeadPoint1 = CGPointMake(circleCenter.x + arrowHeadPointEndRadius * cosf(angle-arrowHeadPointOffsetAngle),
+                                          circleCenter.y + arrowHeadPointEndRadius * sinf(angle-arrowHeadPointOffsetAngle));
+    CGPoint arrowHeadPoint2 = CGPointMake(circleCenter.x + arrowHeadPointEndRadius * cosf(angle+arrowHeadPointOffsetAngle),
+                                          circleCenter.y + arrowHeadPointEndRadius * sinf(angle+arrowHeadPointOffsetAngle));
 
-    // Draw filled circle
-    UIColor *circleColor = [[UIColor greenColor] colorWithAlphaComponent:0.9f];
-
-    CGContextSetFillColorWithColor(context, [circleColor CGColor]);
+    // Draw circle
     CGContextSetLineWidth(context, 1.0f);
     CGContextAddEllipseInRect(context, circleFrame);
-    CGContextDrawPath(context, kCGPathFill);
+    CGContextDrawPath(context, kCGPathStroke);
 
     // Draw arrow
-    CGContextSetStrokeColorWithColor(context, [circleColor CGColor]);
-    CGContextMoveToPoint(context, circleCenter.x, circleCenter.y);
-    CGContextAddLineToPoint(context, arrowEndPoint.x, arrowEndPoint.y);
+    CGContextMoveToPoint(context, arrowEndPoint.x, arrowEndPoint.y);
     CGContextAddLineToPoint(context, arrowHeadPoint1.x, arrowHeadPoint1.y);
     CGContextMoveToPoint(context, arrowEndPoint.x, arrowEndPoint.y);
     CGContextAddLineToPoint(context, arrowHeadPoint2.x, arrowHeadPoint2.y);
     CGContextDrawPath(context, kCGPathStroke);
 
     // Draw label inside circle
-    NSString *label = [NSString stringWithFormat:@"g=%g", self.magnitude];
+    NSString *label = [NSString stringWithFormat:@"%gg", self.magnitude];
     
-    [[UIColor whiteColor] set];
     NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
     [style setAlignment:NSTextAlignmentCenter];
     NSDictionary *attr = @{
                            NSParagraphStyleAttributeName: style,
-                           NSFontAttributeName: [UIFont systemFontOfSize:10.0f]
+                           NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Thin" size:11.0f],
+                           NSForegroundColorAttributeName: [UIColor blueColor],
                            };
 
     CGSize labelSize = [label sizeWithAttributes:attr];
