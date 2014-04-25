df8cd0c6e48a1113fa05b65a33ad5902309ab73d
Chris Miles <miles.chris@gmail.com>
2014-04-25 22:06:43 +1000

    Added icons to ends of the cross fade slider control.

--- DynamicsXray/DynamicsXray/DXRDynamicsXrayConfigurationFaderView.m
+++ DynamicsXray/DynamicsXray/DXRDynamicsXrayConfigurationFaderView.m
@@ -7,6 +7,11 @@
 //
 
 #import "DXRDynamicsXrayConfigurationFaderView.h"
+#import "DXRAssetBytesIconNoOverlay.png.h"
+#import "DXRAssetBytesIconNoOverlay@2x.png.h"
+#import "DXRAssetBytesIconOverlay.png.h"
+#import "DXRAssetBytesIconOverlay@2x.png.h"
+
 
 @interface DXRDynamicsXrayConfigurationFaderView ()
 
@@ -23,27 +28,25 @@
     if (self) {
         UISlider *faderSlider = [[UISlider alloc] initWithFrame:CGRectZero];
 
-        UILabel *faderAppLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        UILabel *faderXrayLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        faderAppLabel.text = @"App";
-        faderXrayLabel.text = @"Xray";
+        UIImageView *iconNoOverlayView = [[UIImageView alloc] initWithImage:[self iconNoOverlayImage]];
+        UIImageView *iconOverlayView = [[UIImageView alloc] initWithImage:[self iconOverlayImage]];
 
+        [self addSubview:iconNoOverlayView];
+        [self addSubview:iconOverlayView];
         [self addSubview:faderSlider];
-        [self addSubview:faderAppLabel];
-        [self addSubview:faderXrayLabel];
 
         faderSlider.translatesAutoresizingMaskIntoConstraints = NO;
-        faderAppLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        faderXrayLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        iconNoOverlayView.translatesAutoresizingMaskIntoConstraints = NO;
+        iconOverlayView.translatesAutoresizingMaskIntoConstraints = NO;
 
         NSDictionary *layoutMetrics = @{@"margin": @(10.0f)};
-        NSDictionary *layoutViews = NSDictionaryOfVariableBindings(faderSlider, faderAppLabel, faderXrayLabel);
+        NSDictionary *layoutViews = NSDictionaryOfVariableBindings(faderSlider, iconNoOverlayView, iconOverlayView);
 
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[faderAppLabel]-(margin)-[faderSlider]-(margin)-[faderXrayLabel]|"
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[iconNoOverlayView]-(margin)-[faderSlider]-(margin)-[iconOverlayView]|"
                                                                      options:NSLayoutFormatAlignAllCenterY
                                                                      metrics:layoutMetrics
                                                                        views:layoutViews]];
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[faderAppLabel]-(>=0)-|"
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[iconNoOverlayView]-(>=0)-|"
                                                                      options:0
                                                                      metrics:layoutMetrics
                                                                        views:layoutViews]];
@@ -51,7 +54,7 @@
                                                                      options:0
                                                                      metrics:layoutMetrics
                                                                        views:layoutViews]];
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[faderXrayLabel]-(>=0)-|"
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[iconOverlayView]-(>=0)-|"
                                                                      options:0
                                                                      metrics:layoutMetrics
                                                                        views:layoutViews]];
@@ -59,6 +62,30 @@
         self.faderSlider = faderSlider;
     }
     return self;
+}
+
+- (UIImage *)iconNoOverlayImage
+{
+    UIScreen *screen = (self.window.screen ?: [UIScreen mainScreen]);
+    CGFloat scale = screen.scale;
+
+    unsigned char *imageBytes = (scale >= 2.0 ? DXRAssetBytesIconNoOverlay2xPNG : DXRAssetBytesIconNoOverlayPNG);
+    unsigned long imageBytesCount = (scale >= 2.0 ? DXRAssetBytesIconNoOverlay2xPNG_size : DXRAssetBytesIconNoOverlayPNG_size);
+    NSData *imageData = [NSData dataWithBytesNoCopy:imageBytes length:imageBytesCount];
+    UIImage *image = [UIImage imageWithData:imageData scale:scale];
+    return image;
+}
+
+- (UIImage *)iconOverlayImage
+{
+    UIScreen *screen = (self.window.screen ?: [UIScreen mainScreen]);
+    CGFloat scale = screen.scale;
+
+    unsigned char *imageBytes = (scale >= 2.0 ? DXRAssetBytesIconOverlay2xPNG : DXRAssetBytesIconOverlayPNG);
+    unsigned long imageBytesCount = (scale >= 2.0 ? DXRAssetBytesIconOverlay2xPNG_size : DXRAssetBytesIconOverlayPNG_size);
+    NSData *imageData = [NSData dataWithBytesNoCopy:imageBytes length:imageBytesCount];
+    UIImage *image = [UIImage imageWithData:imageData scale:scale];
+    return image;
 }
 
 @end
