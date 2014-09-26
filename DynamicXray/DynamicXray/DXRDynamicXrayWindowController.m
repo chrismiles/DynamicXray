23634d2e491c58dd3bbd3a2ac7f5124b29136f36
Krzysztof Zabłocki <krzysztof.zablocki@me.com>
2014-09-26 12:32:34 +0200

    Handling rotation across iOS 7 and 8 correctly

--- DynamicXray/DynamicXray/DXRDynamicXrayWindowController.m
+++ DynamicXray/DynamicXray/DXRDynamicXrayWindowController.m
@@ -209,6 +209,15 @@ static CGFloat
 AngleForUIInterfaceOrientation(UIInterfaceOrientation interfaceOrientation)
 {
     CGFloat angle;
+    BOOL ignoreOrientation = NO;
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
+    if ([[NSProcessInfo processInfo] respondsToSelector:@selector(operatingSystemVersion)]) {
+      ignoreOrientation = YES;
+    }
+#endif
+    if (ignoreOrientation) {
+      return 0;
+    }
 
     if (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
         angle = (CGFloat)M_PI;
